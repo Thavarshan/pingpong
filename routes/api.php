e03845419052215e5385c588aecb5b665fdf1b15
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2020-11-19 02:22:33 +0530

    Update api route

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StudentController;
 
 Route::middleware(['api', 'ext-app'])
-    ->get('/ping', [StudentController::class, 'index']);
+    ->get('/students', [StudentController::class, 'index']);
