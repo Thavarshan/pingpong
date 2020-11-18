5d520b9b1d31209d6e9dc65679f3ee71a6c696a8
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2020-11-19 02:20:24 +0530

    Add status symbol on homescreen

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
-use App\Models\Token;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $token = Token::create([
-        'token' => Str::random(21),
-    ]);
-
-    return $token->token;
+    return 'App working!';
 });
