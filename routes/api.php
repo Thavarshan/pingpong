9e6540dc00b5dbedf4e92a1e40205829a64b9a41
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2020-11-19 12:16:41 +0530

    Update data

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StudentController;
 
-Route::middleware(['api', 'ext-app'])
-    ->get('/students', [StudentController::class, 'index']);
+Route::middleware(['api', 'ext-app'])->get(
+    '/students',
+    [StudentController::class, 'index']
+);
