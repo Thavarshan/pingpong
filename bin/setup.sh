742af15f47f7377183bdc6c90f07aa3f6ef3063b
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-06-17 13:13:33 +0530

    WIP

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,9 +2,9 @@
 
 function main() {
     installProject
-    # if [ $? -eq 0 ]; then
-    #     php artisan blaze:project
-    # fi
+    if [ $? -eq 0 ]; then
+        php artisan blaze:project
+    fi
 }
 
 # Install Composer & JS dependencies, restructure project directories and compile assets.
